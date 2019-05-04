986fcd67a37199efeee8b95615a792cdd0eecb20
Shaiju Rajan <s.rajan@devon.nl>
2019-05-04 19:29:25 +0530

    Single page app secured with Azure AD

--- ProductService/ProductManagementSystem/app/app.js
+++ ProductService/ProductManagementSystem/app/app.js
@@ -1,6 +1,16 @@
-﻿angular.module('pmsApp', ['ngRoute'])
-    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
+﻿angular.module('pmsApp', ['ngRoute', 'AdalAngular'])
+    .config(['$routeProvider', '$locationProvider','adalAuthenticationServiceProvider','$httpProvider', function ($routeProvider, $locationProvider,adalAuthenticationServiceProvider,$httpProvider) {
+
+        $locationProvider.html5Mode(true).hashPrefix('!');
+
+        adalAuthenticationServiceProvider.init({
+            instance:'https://login.microsoftonline.com/',
+            tenant:'shaijurnd.onmicrosoft.com',
+            clientId: 'b5f373c9-78cf-49f1-ab32-ad4fbd186198',
+            endpoints: {
+                'https://localhost:44326/':'https://shaijurnd.onmicrosoft.com/ProductService'
+            }
+        },$httpProvider);
 
         $routeProvider.when('/home', {
             templateUrl:'app/home/views/home.view.html'
@@ -8,9 +18,10 @@
         .when('/products', {
             templateUrl: '/app/products/views/products.view.html',
             controller: 'productsController',
-            controllerAs:'vm'
+            controllerAs:'vm',
+            requireADLogin:true
         })
         .otherwise({
-            redirectTo:'/home'
+            redirectTo:'/products'
         });
     }]);
