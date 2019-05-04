b6f2bd9b4cdf0dd720911e4f6dd18c52ad9b4850
Shaiju Rajan <s.rajan@devon.nl>
2019-05-04 19:36:45 +0530

    Added Token generator solution

---
+++ AzureADTokenGenerator/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace AzureADTokenGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTenant = new System.Windows.Forms.TextBox();
+            this.textBoxClientId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAppIdUri = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPwd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxToken = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ButtonGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tenant";
+            // 
+            // textBoxTenant
+            // 
+            this.textBoxTenant.Location = new System.Drawing.Point(77, 21);
+            this.textBoxTenant.Name = "textBoxTenant";
+            this.textBoxTenant.Size = new System.Drawing.Size(314, 20);
+            this.textBoxTenant.TabIndex = 1;
+            // 
+            // textBoxClientId
+            // 
+            this.textBoxClientId.Location = new System.Drawing.Point(77, 47);
+            this.textBoxClientId.Name = "textBoxClientId";
+            this.textBoxClientId.Size = new System.Drawing.Size(314, 20);
+            this.textBoxClientId.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ClientId";
+            // 
+            // textBoxAppIdUri
+            // 
+            this.textBoxAppIdUri.Location = new System.Drawing.Point(77, 73);
+            this.textBoxAppIdUri.Name = "textBoxAppIdUri";
+            this.textBoxAppIdUri.Size = new System.Drawing.Size(314, 20);
+            this.textBoxAppIdUri.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "AppId Uri";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(77, 99);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(314, 20);
+            this.textBoxUserName.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "User";
+            // 
+            // textBoxPwd
+            // 
+            this.textBoxPwd.Location = new System.Drawing.Point(77, 125);
+            this.textBoxPwd.Name = "textBoxPwd";
+            this.textBoxPwd.Size = new System.Drawing.Size(314, 20);
+            this.textBoxPwd.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Pwd";
+            // 
+            // textBoxToken
+            // 
+            this.textBoxToken.Location = new System.Drawing.Point(77, 222);
+            this.textBoxToken.Name = "textBoxToken";
+            this.textBoxToken.Size = new System.Drawing.Size(314, 20);
+            this.textBoxToken.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Token";
+            // 
+            // ButtonGenerate
+            // 
+            this.ButtonGenerate.Location = new System.Drawing.Point(316, 161);
+            this.ButtonGenerate.Name = "ButtonGenerate";
+            this.ButtonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGenerate.TabIndex = 12;
+            this.ButtonGenerate.Text = "Generate";
+            this.ButtonGenerate.UseVisualStyleBackColor = true;
+            this.ButtonGenerate.Click += new System.EventHandler(this.ButtonGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 258);
+            this.Controls.Add(this.ButtonGenerate);
+            this.Controls.Add(this.textBoxToken);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxPwd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxAppIdUri);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxClientId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxTenant);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTenant;
+        private System.Windows.Forms.TextBox textBoxClientId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAppIdUri;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPwd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxToken;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ButtonGenerate;
+    }
+}
+
---
+++ AzureADTokenGenerator/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace AzureADTokenGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGenerate_Click(object sender, EventArgs e)
+        {
+            var authority = string.Format("https://login.microsoftonline.com/{0}", textBoxTenant.Text);
+            var authenticationContext = new AuthenticationContext(authority);
+
+            var userCredentials = new UserCredential(textBoxUserName.Text, textBoxPwd.Text);
+
+            textBoxToken.Text = "Bearer " + authenticationContext.AcquireToken(textBoxAppIdUri.Text, textBoxClientId.Text, userCredentials).AccessToken;
+
+            Clipboard.SetText(textBoxToken.Text);
+        }
+    }
+}
